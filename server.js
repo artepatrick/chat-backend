a2985128cc7543977b2b04667954cb6a58c5ae59
artepatrick <artepatrick@gmail.com>
2025-03-09 12:09:43 -0300

    change server port to 8080 and listen on all interfaces

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+http.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
