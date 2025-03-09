f5e87a9a716ceae752dfd1ab9f7f20c678362f23
artepatrick <artepatrick@gmail.com>
2025-03-09 12:41:30 -0300

    fix: update CORS configuration to restrict origin and methods

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
-  cors: { origin: "*" },
+  cors: {
+    origin: "https://chat-frontend.fly.dev",
+    methods: ["GET", "POST"],
+  },
 });
 
 // Rota simples para teste
